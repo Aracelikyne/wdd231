d32d9bdf53e7dfbd66d273a298da4458ecdc81b8
Monica "Araceli" Kyne <aracelikyne@gmail.com>
2025-05-10 11:43:39 -0500

    adjusted css js and html files

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,3 +1,17 @@
-document.querySelector(".menu-icon").addEventListener("click", function() {
-    document.querySelector("nav").classList.toggle("active");
+
+document.addEventListener("DOMContentLoaded", function () {
+    const menuButton = document.getElementById("menu-toggle");
+    const navList = document.querySelector("#main-nav ul");
+
+    menuButton.addEventListener("click", function () {
+        if (navList.classList.contains("hide-menu")) {
+            navList.classList.remove("hide-menu");
+            navList.classList.add("show-menu");
+            menuButton.textContent = "X";
+            }else{
+                navList.classList.remove("show-menu");
+                navList.classList.add("hide-menu");
+                menuButton.textContent = "☰";
+            }
+    });
 });
